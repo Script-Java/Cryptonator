e745eedc04b6fb043fef2702e6280d2ceaaec267
Atrin Shahroudi <atrindev@gmail.com>
2024-06-06 11:02:09 -0500

    Normalize all the line endings

---
+++ backend/app.py
@@ -0,0 +1,1 @@
+# here will be backend/ AI based
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="">
+      Hello world!
+    </div>
+    </>
+  )
+}
+
+export default App
